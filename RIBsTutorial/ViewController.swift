82de6fc94640d70a9eb801d2fdc748a616ca18f3
tahara yo <tahara.you@ring-tech.jp>
2024-07-02 08:44:56 +0900

    Initial Commit

---
+++ RIBsTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBsTutorial
+//
+//  Created by yotahara on 2024/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
